5fdb8f334f71764c5b45a402ed0d3c34870d358d
youngjin <daphnejin36@hanmail.net>
2022-03-30 14:50:51 +0900

    youngjin
    write all

--- To_do_list/src/main/java/com/javalec/project/command/CwriteCommand.java
+++ To_do_list/src/main/java/com/javalec/project/command/CwriteCommand.java
@@ -3,12 +3,19 @@ package com.javalec.project.command;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.javalec.project.dao.Write_dao;
+
 public class CwriteCommand implements PCommand {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		
+//		String wName = request.getParameter("wName");
+		String wContent = request.getParameter("wContent");
+		String wFinish = request.getParameter("wFinish");
+	
+		Write_dao dao = new Write_dao();
+		dao.cwrite( wContent , wFinish);
+
 	}
 
 }
