8f44ad8682492e9b4f3202c0c90f9557d7a686da
김은애 <100197284+kim0502@users.noreply.github.com>
2022-03-30 15:53:18 +0900

    kimeunae
    모두 넣었습니다.

--- To_do_list/src/main/java/com/javalec/project/dao/Detaillist_dao.java
+++ To_do_list/src/main/java/com/javalec/project/dao/Detaillist_dao.java
@@ -1,5 +1,73 @@
 package com.javalec.project.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.javalec.project.dto.Detaillist_dto;
+
 public class Detaillist_dao {
 
+	DataSource dataSource; // Servers-context.xml을 가져올거임
+
+	public Detaillist_dao() {
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/to_do_list");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 전체검색
+	public ArrayList<Detaillist_dto> list() {
+		ArrayList<Detaillist_dto> dtos = new ArrayList<Detaillist_dto>();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
+			connection = dataSource.getConnection();
+			String query = "select  wName, wContent, wFinish from td_write";
+			preparedStatement = connection.prepareStatement(query);
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				
+				String wName = resultSet.getString("wName");
+				String wContent = resultSet.getString("wContent");
+				String wFinish = resultSet.getString("wFinish");
+			
+				Detaillist_dto dto = new Detaillist_dto(wName, wContent, wFinish);
+				dtos.add(dto);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally { // 이상이 있거나 없거나 온다.
+			try {
+				if (resultSet != null)
+					resultSet.close();
+				if (preparedStatement != null)
+					preparedStatement.close();
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+
+		return dtos;
+
+	
+
+	
+	}	
 }
--- To_do_list/src/main/java/com/javalec/project/dto/Detaillist_dto.java
+++ To_do_list/src/main/java/com/javalec/project/dto/Detaillist_dto.java
@@ -1,5 +1,53 @@
 package com.javalec.project.dto;
 
+
+
 public class Detaillist_dto {
+	//Field
+	
+	String wName;
+	
+	String wContent;
+	
+	String wFinish;
+
+	//constructor
+	public Detaillist_dto(){
+		
+	}
+
+	public Detaillist_dto(String wName, String wContent, String wFinish) {
+		super();
+		this.wName = wName;
+		this.wContent = wContent;
+		this.wFinish = wFinish;
+
+	
+}
+	//Method
+
+	public String getwName() {
+		return wName;
+	}
+
+	public void setwName(String wName) {
+		this.wName = wName;
+	}
+
+	public String getwContent() {
+		return wContent;
+	}
+
+	public void setwContent(String wContent) {
+		this.wContent = wContent;
+	}
+
+	public String getwFinish() {
+		return wFinish;
+	}
+
+	public void setwFinish(String wFinish) {
+		this.wFinish = wFinish;
+	}
 
 }
